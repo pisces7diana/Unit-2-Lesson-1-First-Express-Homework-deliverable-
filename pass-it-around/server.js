9b11396cfe55cb6a1d570c33a65bf6aea98ca213
pisces7diana <email7diana@gmail.com>
2023-12-15 23:38:58 -0800

    Take one Down and Pass it Around - but I'm confused with the port - it was at port 3000, but then it kept changing to from 5500-5103

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,1 +1,20 @@
-console.log('hi')
+const express = require("express")
+
+const app = express()
+
+app.get('/', (res, req) => {
+    res.send("<h1>99 Bottles of beer on the wall</h1><a href='/98'>Take one down, pass it around</a>")
+})
+
+app.get('/:numberOfBottles', (res, req) => {
+    const numberOfBottles = req.params.numberOfBottles
+    if (numberOfBottles > 0) {
+        res.send(`<h2>${numberOfBottles} Bottles of beer on the wall.</h2><a href="/${numberOfBottles - 1}">Take one down, pass it around</a>`)
+      } else {
+        res.send("<h2>0 bottles left on the wall!</h2><a href="/">I'm way too tipsy, but I'm here to party so let's start over</a>");
+      }
+    })
+   
+app.listen(3000, () => {
+    console.log('server is listening on port 3000')
+})
--- server.js
+++ server.js
@@ -30,9 +30,28 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 
 app.get('/magic/:question', (req, res) => {
     const question = req.params.question
-    const randomMagicBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const randomMagicBall =
+        ["It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it", 
+        "As I see it yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"]
+
     const response = randomMagicBall[Math.floor(Math.random() * randomMagicBall.length)];
     res.send(`<h1>Question: ${question}</h1><h1>Response: ${response}</h1>`);
-
-
 })
