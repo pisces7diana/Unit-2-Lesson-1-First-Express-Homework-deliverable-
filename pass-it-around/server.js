6958ba03a09fcf818380136eb8f3ea71e143d6a3
pisces7diana <email7diana@gmail.com>
2023-12-16 13:57:35 -0800

    Take one Down and Pass it Around Completed

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -2,19 +2,21 @@ const express = require("express")
 
 const app = express()
 
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
     res.send("<h1>99 Bottles of beer on the wall</h1><a href='/98'>Take one down, pass it around</a>")
 })
 
-app.get('/:numberOfBottles', (res, req) => {
+app.get('/:numberOfBottles', (req, res) => {
     const numberOfBottles = req.params.numberOfBottles
     if (numberOfBottles > 0) {
         res.send(`<h2>${numberOfBottles} Bottles of beer on the wall.</h2><a href="/${numberOfBottles - 1}">Take one down, pass it around</a>`)
       } else {
         res.send("<h2>0 bottles left on the wall!</h2><a href="/">I'm way too tipsy, but I'm here to party so let's start over</a>");
       }
-    })
-   
-app.listen(3000, () => {
-    console.log('server is listening on port 3000')
+})
+
+const PORT = 5503
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`)
 })
